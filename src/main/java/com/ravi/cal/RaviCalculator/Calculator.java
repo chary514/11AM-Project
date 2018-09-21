e0832892445125fe934a07f28401469d1738730d
devops <devopsguy9@gmail.com>
2018-09-21 11:15:59 +0530

    Calculator Started

---
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.ravi.cal.RaviCalculator;
+
+import java.util.Scanner;
+
+public class Calculator  {
+	
+	private long first;
+	private long second;
+	
+	public long getFirst() {
+		return first;
+	}
+
+	public void setFirst(long first) {
+		this.first = first;
+	}
+
+	public long getSecond() {
+		return second;
+	}
+
+	public void setSecond(long second) {
+		this.second = second;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Implmenet Calculator Functionality");
+		
+	}
+	 
+
+}
